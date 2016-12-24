227df7bd770f3e5ddb650fe7ad90e47cef3fa33f
Adar Wesley <adar.wesley@gmail.com>
2016-12-24 17:13:11 +0200

    Load StateMachine from Model successful

--- statemachine/src/test/java/org/awesley/samples/StatemachineApplicationTests.java
+++ statemachine/src/test/java/org/awesley/samples/StatemachineApplicationTests.java
@@ -12,6 +12,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.ObjectStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineBuilder;
 import org.springframework.statemachine.config.StateMachineFactory;
 import org.springframework.statemachine.config.model.StateMachineModel;
 import org.springframework.statemachine.uml.UmlStateMachineModelFactory;
@@ -38,15 +40,19 @@ public class StatemachineApplicationTests {
 
 	@Test
 	public void loadStateMachineModel(){
-		ResourceSet resourceSet = new ResourceSetImpl();
-		UMLPackage umlPackage = UMLPackage.eINSTANCE;
-		resourceSet.getPackageRegistry().put(umlPackage.getNsURI(), mlPackage);
+//		ResourceSet resourceSet = new ResourceSetImpl();
+//		UMLPackage umlPackage = UMLPackage.eINSTANCE;
+//		resourceSet.getPackageRegistry().put(umlPackage.getNsURI(), umlPackage);
 		
 		UmlStateMachineModelFactory umlStateMachineModelFactory;
 		
 		umlStateMachineModelFactory = new UmlStateMachineModelFactory(new ClassPathResource("StateMachine2.uml"));
 		StateMachineModel<String, String> stateMachineModel = umlStateMachineModelFactory.build();
-		
 		assertNotNull(stateMachineModel);
+		
+		ObjectStateMachineFactory<String, String> factory = new ObjectStateMachineFactory<>(stateMachineModel);
+		
+		StateMachine<String, String> stateMachine = factory.getStateMachine();
+		assertNotNull(stateMachine);
 	}
 }
