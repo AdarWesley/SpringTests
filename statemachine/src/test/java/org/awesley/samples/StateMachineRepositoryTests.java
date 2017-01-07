58ce710f54ba5e8c0316be27141cdc206fc35b54
Adar Wesley <adar.wesley@gmail.com>
2017-01-07 11:18:48 +0200

    Added test stateMachineActionGetsThreadContext to verify an Action runs on the same thread, and therefore, has the context available.

--- statemachine/src/test/java/org/awesley/samples/StateMachineRepositoryTests.java
+++ statemachine/src/test/java/org/awesley/samples/StateMachineRepositoryTests.java
@@ -15,8 +15,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.data.ActionRepository;
 import org.springframework.statemachine.data.StateRepository;
 import org.springframework.statemachine.data.TransitionRepository;
 import org.springframework.statemachine.data.jpa.JpaRepositoryAction;
@@ -27,11 +32,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StateMachineRepositoryTests {
+	
+	@Autowired
+	ConfigurableApplicationContext context;
+	
 	@Autowired
 	StateRepository<JpaRepositoryState> stateRepository;
 
 	@Autowired
 	TransitionRepository<JpaRepositoryTransition> transitionRepository;
+	
+	@Autowired
+	ActionRepository<JpaRepositoryAction> actionRepository;
 	
 	@Autowired
 	StateMachineFactory<String, String> stateMachineRepositoryFactory;
@@ -46,13 +58,15 @@ public class StateMachineRepositoryTests {
 		JpaRepositoryAction action = new JpaRepositoryAction();
 		action.setName("S1ToS2TransitionAction");
 		
+		actionRepository.save(action);
+		
 		actionSet.add(action);
-		transitionRepository.save(new JpaRepositoryTransition(statesMap.get("S1"), statesMap.get("S2"), "E1"));
+		transitionRepository.save(new JpaRepositoryTransition("Proposal", statesMap.get("S1"), statesMap.get("S2"), "E1", actionSet));
 	}
 
 	private Map<String, JpaRepositoryState> initializeStates(String initialState, List<String> states) {
 		Map<String, JpaRepositoryState> statesMap = new HashMap<String, JpaRepositoryState>();
-		states.forEach(name -> statesMap.put(name, new JpaRepositoryState(name, name == initialState)));
+		states.forEach(name -> statesMap.put(name, new JpaRepositoryState("Proposal", name, name == initialState)));
 		stateRepository.save(statesMap.values());
 		
 		return statesMap;
@@ -63,9 +77,35 @@ public class StateMachineRepositoryTests {
 	}
 
 	@Test
-	public void test() {
-		StateMachine<String, String> stateMachine = stateMachineRepositoryFactory.getStateMachine();
+	public void createStateMachineFromRepository() {
+		StateMachine<String, String> stateMachine = stateMachineRepositoryFactory.getStateMachine("Proposal");
 		assertNotNull(stateMachine);
 	}
+	
+	@Test
+	public void stateMachineActionGetsThreadContext() {
+		ThreadIdAction threadIdAction = new ThreadIdAction();
+		context.getBeanFactory().registerSingleton("S1ToS2TransitionAction", threadIdAction);
+		
+		StateMachine<String, String> stateMachine = stateMachineRepositoryFactory.getStateMachine("Proposal");
+		stateMachine.start();
+		stateMachine.sendEvent("E1");
+		
+		
+		assertEquals(Thread.currentThread().getId(), threadIdAction.getActionThreadId());
+	}
 
+	private class ThreadIdAction implements Action<String, String> {
+
+		private long actionThreadId = 0;
+		
+		@Override
+		public void execute(StateContext<String, String> context) {
+			actionThreadId = Thread.currentThread().getId();
+		}
+
+		public long getActionThreadId() {
+			return actionThreadId;
+		}
+	}
 }
